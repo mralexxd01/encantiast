96249c3df828439ad030e99a7f9ffea7534d949c
MrAlexXD_ <alexmfa011@gmail.com>
2026-07-02 21:53:55 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // ⚠️ AGREGA ESTA LÍNEA (Cambia la URL por tu dominio real cuando lo compres o el de Vercel/Netlify)
+  site: 'https://encantia.xyz', 
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
